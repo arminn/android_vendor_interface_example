5d6ab9d32125d89b90bafed0c96b6cbaad238bd5
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2021-12-24 01:19:00 +0200

    Add default implementation of the AIDL service
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

---
+++ aidl/default/service.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2021 EPAM Systems Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/logging.h>
+#include <log/log.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+#include "Virt.h"
+
+using ::aidl::epam::virt::Virt;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<Virt> v = ndk::SharedRefBase::make<Virt>();
+
+    const std::string instance = std::string() + Virt::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(v->asBinder().get(), instance.c_str());
+    CHECK(status == STATUS_OK);
+    ALOGI("AIDL Virt service started !");
+    ABinderProcess_joinThreadPool();
+    return -1; // Should never be reached
+}
